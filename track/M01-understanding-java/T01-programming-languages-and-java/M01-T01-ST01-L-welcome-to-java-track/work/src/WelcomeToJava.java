9804d6f2dc7b8adb74aa2a68bae4cb3302b998fe
akshata <karajagiakshata@gmail.com>
2026-08-04 21:34:39 +0530

    D2change

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -6,6 +6,6 @@ public class WelcomeToJava {
         System.out.println("welcome to the java track.");
         System.out.println("I really know how to build programming logic.");
         System.out.println("Now I am learning how to express that logic using java.");
-        System.err.println("Now I know how to use gitgit and github");
+        System.err.println("Now I know how to use git and github");
     }
 }
