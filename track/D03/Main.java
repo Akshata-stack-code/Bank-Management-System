e479e2a0de99b7a6b8bf62a73721e957d7b4b76b
akshata <karajagiakshata@gmail.com>
2026-08-04 11:33:45 +0530

    Day3changes

---
+++ track/D03/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.println("TuesdayWednesday");
+        System.out.println("Thrusday");
+        System.out.println("FridaySaturday");
+        System.out.println("Sunday");
+
+    }
+}
--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,11 +2,10 @@
 public class WelcomeToJava {
 
     public static void main(String[] args) {
-        //Write your code here
-        // Day 1 changes
+        //Write your code heregit
         System.out.println("welcome to the java track.");
         System.out.println("I really know how to build programming logic.");
         System.out.println("Now I am learning how to express that logic using java.");
-        System.err.println("Now I know how to usegit and github");
+        System.err.println("Now I know how to use gitgit and github");
     }
 }
