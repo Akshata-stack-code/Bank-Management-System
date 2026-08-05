094b0ac057227785515ba9032eda47609bff79c1
akshata <karajagiakshata@gmail.com>
2026-08-05 12:42:12 +0530

    pg3

---
+++ track/M02/pg3.java
@@ -0,0 +1,11 @@
+
+public class pg3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.err.println(i);
+            i++;
+        }
+    }
+}
