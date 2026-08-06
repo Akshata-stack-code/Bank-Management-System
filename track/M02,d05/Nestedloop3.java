fc2ba34046e4cc7c784f21614c187ed0673fad91
akshata <karajagiakshata@gmail.com>
2026-08-06 12:29:24 +0530

    break

---
+++ track/M02,d05/Nestedloop3.java
@@ -0,0 +1,16 @@
+
+public class Nestedloop3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            int j = 1;
+            do {
+                System.out.println(j);
+                j++;.....polo
+            } while (i <= 5);
+            System.out.println();
+            i++;
+        } while (i <= 5);
+    }
+}
