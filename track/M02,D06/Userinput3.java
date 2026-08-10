2b494af24de8258d8403a87d4666183cabd4805f
akshata <karajagiakshata@gmail.com>
2026-08-10 13:14:50 +0530

    Student

--- track/M02,D06/Userinput3.java
+++ track/M02,D06/Userinput3.java
@@ -6,7 +6,7 @@ public class Userinput3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("ENter char :"); //Akshata
-        char ch = scan.next().charAt(5);
+        char ch = scan.next().charAt(2);
         System.out.println(ch);
     }
 }
